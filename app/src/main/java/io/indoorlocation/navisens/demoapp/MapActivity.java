f5c33ef747d28f5d42732f3722c0addd4ee0deb0
Thierry Levisse <thierry@mapwize.io>
2018-05-15 17:27:45 +0200

    Update dependencies

--- app/src/main/java/io/indoorlocation/navisens/demoapp/MapActivity.java
+++ app/src/main/java/io/indoorlocation/navisens/demoapp/MapActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
@@ -51,7 +50,7 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
 
-                mapwizePlugin = new MapwizePlugin(mapView, mapboxMap, new MapOptions());
+                mapwizePlugin = new MapwizePlugin(mapView, new MapOptions());
                 startLocationService();
 
                 mapwizePlugin.setOnMapClickListener(new MapwizePlugin.OnMapClickListener() {
