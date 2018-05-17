3bc96c32d1fba4de86260cf9f5904443b59dfcd0
Thierry Levisse <pougabra@gmail.com>
2018-05-17 12:41:50 +0200

    Alpha (#1)
    1.0.0

---
+++ app/src/main/java/io/indoorlocation/navisens/demoapp/MapActivity.java
@@ -0,0 +1,138 @@
+package io.indoorlocation.navisens.demoapp;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.navisens.motiondnaapi.MotionDnaApplication;
+
+import io.indoorlocation.core.IndoorLocation;
+import io.indoorlocation.core.IndoorLocationProvider;
+import io.indoorlocation.manual.ManualIndoorLocationProvider;
+import io.indoorlocation.navisens.NavisensIndoorLocationProvider;
+import io.mapwize.mapwizeformapbox.MapOptions;
+import io.mapwize.mapwizeformapbox.MapwizePlugin;
+import io.mapwize.mapwizeformapbox.model.LatLngFloor;
+
+public class MapActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private MapwizePlugin mapwizePlugin;
+    private NavisensIndoorLocationProvider navisensIndoorLocationProvider;
+    private static final int REQUEST_MDNA_PERMISSIONS = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoibWFwd2l6ZSIsImEiOiJjamNhYnN6MjAwNW5pMnZvMnYzYTFpcWVxIn0.veTCqUipGXCw8NwM2ep1Xg");// PASTE YOU MAPBOX API KEY HERE !!! This is a demo key. It is not allowed to use it for production. The key might change at any time without notice. Get your key by signing up at mapbox.com
+        setContentView(R.layout.activity_map);
+
+        mapView = findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        final IndoorLocationProvider manualIndoorLocationProvider = new ManualIndoorLocationProvider();
+
+        navisensIndoorLocationProvider = new NavisensIndoorLocationProvider(getApplicationContext(), manualIndoorLocationProvider,"uu6oF6dDdNsIBWBez4pw2GuMwNWGJlLpRjVjsa4c23XrT8wqT7BKnXS7WuWSyPfc");
+
+
+        MapOptions opts = new MapOptions.Builder().build();
+        mapwizePlugin = new MapwizePlugin(mapView, opts);
+        mapwizePlugin.setOnDidLoadListener(new MapwizePlugin.OnDidLoadListener() {
+            @Override
+            public void didLoad(MapwizePlugin plugin) {
+
+                startLocationService();
+
+                mapwizePlugin.setOnMapClickListener(new MapwizePlugin.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLngFloor latLngFloor) {
+
+                        Location location = new Location(navisensIndoorLocationProvider.getName());
+                        location.setLatitude(latLngFloor.getLatitude());
+                        location.setLongitude(latLngFloor.getLongitude());
+                        IndoorLocation indoorLocation = new IndoorLocation(navisensIndoorLocationProvider.getName(), latLngFloor.getLatitude(), latLngFloor.getLongitude(), latLngFloor.getFloor(), System.currentTimeMillis());
+                        manualIndoorLocationProvider.dispatchIndoorLocationChange(indoorLocation);
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void setupLocationProvider() {
+        navisensIndoorLocationProvider.start();
+        mapwizePlugin.setLocationProvider(navisensIndoorLocationProvider);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (MotionDnaApplication.checkMotionDnaPermissions(this) == true) {
+            setupLocationProvider();
+        }
+    }
+
+    private void startLocationService() {
+        if  (
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED
+            ) {
+            ActivityCompat.requestPermissions(this, MotionDnaApplication.needsRequestingPermissions(), REQUEST_MDNA_PERMISSIONS);
+        }
+        else {
+            setupLocationProvider();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
---
+++ navisenslocationprovider/src/main/java/io/indoorlocation/navisens/NavisensIndoorLocationProvider.java
@@ -0,0 +1,117 @@
+package io.indoorlocation.navisens;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import com.navisens.motiondnaapi.MotionDna;
+import com.navisens.motiondnaapi.MotionDnaApplication;
+import com.navisens.motiondnaapi.MotionDnaInterface;
+
+import java.util.Map;
+
+import io.indoorlocation.core.IndoorLocation;
+import io.indoorlocation.core.IndoorLocationProvider;
+import io.indoorlocation.core.IndoorLocationProviderListener;
+
+public class NavisensIndoorLocationProvider extends IndoorLocationProvider implements MotionDnaInterface, IndoorLocationProviderListener {
+
+    private boolean mStarted = false;
+
+    private MotionDnaApplication mMotionDna;
+    private Context mContext;
+    private Double mCurrentFloor = null;
+    private String mNavisensKey;
+    private IndoorLocationProvider mSourceProvider;
+
+    public NavisensIndoorLocationProvider(Context context, IndoorLocationProvider sourceProvider, String navisensKey) {
+        super();
+        mContext = context;
+        mMotionDna = new MotionDnaApplication(this);
+        mSourceProvider = sourceProvider;
+        mSourceProvider.addListener(this);
+        mNavisensKey = navisensKey;
+    }
+
+    @Override
+    public boolean supportsFloor() {
+        return true;
+    }
+
+    @Override
+    public void start() {
+        if (!mStarted) {
+            mStarted = true;
+            mMotionDna.setCallbackUpdateRateInMs(1000);
+            mMotionDna.setPowerMode(MotionDna.PowerConsumptionMode.PERFORMANCE);
+            mMotionDna.runMotionDna(mNavisensKey);
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (mStarted) {
+            mStarted = false;
+            mMotionDna.stop();
+        }
+    }
+
+    @Override
+    public boolean isStarted() {
+        return mStarted;
+    }
+
+    @Override
+    public void receiveMotionDna(MotionDna motionDna) {
+        MotionDna.Location location = motionDna.getLocation();
+        IndoorLocation indoorLocation = new IndoorLocation(getName(), location.globalLocation.latitude, location.globalLocation.longitude, mCurrentFloor, System.currentTimeMillis());
+        dispatchIndoorLocationChange(indoorLocation);
+    }
+
+    @Override
+    public void receiveNetworkData(MotionDna motionDna) {
+
+    }
+
+    @Override
+    public void receiveNetworkData(MotionDna.NetworkCode networkCode, Map<String, ?> map) {
+
+    }
+
+    @Override
+    public void reportError(MotionDna.ErrorCode errorCode, String s) {
+        this.dispatchOnProviderError(new Error(errorCode.toString() + " " + s));
+    }
+
+    @Override
+    public Context getAppContext() {
+        return mContext.getApplicationContext();
+    }
+
+    @Override
+    public PackageManager getPkgManager() {
+        return mContext.getPackageManager();
+    }
+
+    @Override
+    public void onProviderStarted() {
+        this.dispatchOnProviderStarted();
+    }
+
+    @Override
+    public void onProviderStopped() {
+        this.dispatchOnProviderStopped();
+    }
+
+    @Override
+    public void onProviderError(Error error) {
+       dispatchOnProviderError(error);
+    }
+
+    @Override
+    public void onIndoorLocationChange(IndoorLocation indoorLocation) {
+        dispatchIndoorLocationChange(indoorLocation);
+        mCurrentFloor = indoorLocation.getFloor();
+        mMotionDna.setLocationLatitudeLongitude(indoorLocation.getLatitude(), indoorLocation.getLongitude());
+        mMotionDna.setHeadingMagInDegrees();
+    }
+}
