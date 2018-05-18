fe4acca91658d5053332edd95ddd1dff5bf7c047
Thierry Levisse <thierry@mapwize.io>
2018-05-18 11:36:58 +0200

    Mapbox key removed

--- app/src/main/java/io/indoorlocation/navisens/demoapp/MapActivity.java
+++ app/src/main/java/io/indoorlocation/navisens/demoapp/MapActivity.java
@@ -32,7 +32,7 @@ public class MapActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, "pk.eyJ1IjoibWFwd2l6ZSIsImEiOiJjamNhYnN6MjAwNW5pMnZvMnYzYTFpcWVxIn0.veTCqUipGXCw8NwM2ep1Xg");// PASTE YOU MAPBOX API KEY HERE !!! This is a demo key. It is not allowed to use it for production. The key might change at any time without notice. Get your key by signing up at mapbox.com
+        Mapbox.getInstance(this, "pk.mapwize");
         setContentView(R.layout.activity_map);
 
         mapView = findViewById(R.id.mapview);
